0a914359bf1e2de1c79bdbac720be613e72c9c10
jakobottar <jakobottar@pm.me>
2021-05-26 12:50:46 -0600

    added random train/test split

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader, SubsetRandomSampler
 from torchvision import datasets, transforms, models
+import torchdata as td
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,14 +16,16 @@ transform = transforms.Compose([transforms.Resize(255),
                                 transforms.CenterCrop(224),
                                 transforms.ToTensor()])
 
-numFiles = 2000
-indices = np.arange(0, numFiles)
-np.random.shuffle(indices)
+data = td.datasets.WrapDataset(datasets.ImageFolder('dvc', transform=transform))
 
-data = datasets.ImageFolder('dvc', transform=transform)
+total_count = len(data)
+train_count = int(0.8 * total_count)
+test_count = total_count - train_count
 
-train_dataloader = DataLoader(data, batch_size=32, sampler=SubsetRandomSampler(indices[:1500]))
-test_dataloader = DataLoader(data, batch_size=32, sampler=SubsetRandomSampler(indices[-500:]))
+train_data, test_data = torch.utils.data.random_split(data, (train_count, test_count)) # this can be extended to include a validation set too
+
+train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=32, shuffle=True)
 
 # images, labels = next(iter(dataloader))
 # im_ = images[0].squeeze()[0]
@@ -50,7 +53,7 @@ def train(dataloader, model, loss_fn, optimizer):
 
         # print(batch)
 
-        if batch % 8 == 0:
+        if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
             print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
